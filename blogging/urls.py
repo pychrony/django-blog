e73b19f2903727a9f7ff423cdb528184c3e802be
pychrony <grasps.keys-0x@icloud.com>
2025-01-12 11:22:53 -0500

    Changes to conform to Assignment 8

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from blogging.views import list_view, detail_view
+#from blogging.views import list_view, detail_view
+from blogging.views import PostListView, PostDetailView, StubView
 
 urlpatterns = [
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('', PostListView.as_view(), name="blog_index"),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name="blog_detail"),
+    path('stub/', StubView.as_view(), name = 'stub_view'),
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -1,17 +1,39 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
+from django.views.generic import ListView, DetailView, View
 from blogging.models import Post
-from django.shortcuts import render
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+
+class StubView(View):
+    def get(self, request, *args, **kwargs):
+        body = "Stub View\n\n"
+        if args:
+            body += "Args:\n"
+            body += "\n".join(["\t%s" % a for a in args])
+        if kwargs:
+            body += "Kwargs:\n"
+            body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+        return HttpResponse(body, content_type = "text/plain")
+
+class PostListView(ListView):
+    template_name = 'blogging/list.html'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    template_name = 'blogging/detail.html'
+
+    def get_queryset(self):
+        return Post.objects.exclude(published_date__exact=None)
+
+
+# def stub_view(request, *args, **kwargs):
+#     body = "Stub View\n\n"
+#     if args:
+#         body += "Args:\n"
+#         body += "\n".join(["\t%s" % a for a in args])
+#     if kwargs:
+#         body += "Kwargs:\n"
+#         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+#     return HttpResponse(body, content_type="text/plain")
 
 # def list_view(request):
 #     published = Post.objects.exclude(published_date__exact=None)
@@ -22,17 +44,17 @@ def stub_view(request, *args, **kwargs):
 #     return HttpResponse(body, content_type="text/html")
 
 # rewrite our view
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     context = {'posts': posts}
+#     return render(request, 'blogging/list.html', context)
+#
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
