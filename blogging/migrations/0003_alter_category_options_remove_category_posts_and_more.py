1a69513b5770afd792cd6004fe57990e581a19bb
pychrony <grasps.keys-0x@icloud.com>
2025-01-18 09:33:27 -0500

    Applied black.

--- blogging/migrations/0003_alter_category_options_remove_category_posts_and_more.py
+++ blogging/migrations/0003_alter_category_options_remove_category_posts_and_more.py
@@ -6,21 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blogging', '0002_category'),
+        ("blogging", "0002_category"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='category',
-            options={'verbose_name_plural': 'Categories'},
+            name="category",
+            options={"verbose_name_plural": "Categories"},
         ),
         migrations.RemoveField(
-            model_name='category',
-            name='posts',
+            model_name="category",
+            name="posts",
         ),
         migrations.AddField(
-            model_name='post',
-            name='categories',
-            field=models.ManyToManyField(blank=True, related_name='posts', to='blogging.category'),
+            model_name="post",
+            name="categories",
+            field=models.ManyToManyField(
+                blank=True, related_name="posts", to="blogging.category"
+            ),
         ),
     ]
